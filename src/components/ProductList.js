41693f4b3dc80c80b02f0e207c4a7c21cffdbaee
rebeccalifetrace <92694849+rebeccalifetrace@users.noreply.github.com>
2021-10-17 17:43:37 -0400

    Add files via upload
    cartContext.js with error 'not valid react child'

---
+++ src/components/ProductList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { FaHtml5 } from 'react-icons/fa'
+import { useFilterContext } from '../context/filter_context'
+import GridView from './GridView'
+import ListView from './ListView'
+
+const ProductList = () => {
+  const {filtered_products: products, grid_view}=useFilterContext()
+  if(products.length<1){
+    return (
+      <h5> No matching products...</h5>
+    )
+  }
+  return (<>
+    {grid_view?<GridView products={products}/>:<ListView products={products} />}
+  
+  </>)
+}
+
+export default ProductList
---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FeaturedProducts, Hero, Services, Contact } from '../components'
+const HomePage = () => {
+  return (
+  // <h4>home page</h4>
+  <>
+    <Hero/>
+    <FeaturedProducts/>
+    <Services/>
+    <Contact/>
+  </>
+  )
+}
+
+export default HomePage
